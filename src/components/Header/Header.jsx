ac5ae5056c223ce94d48813c0f36a43094db0896
Hollanda <joao.martinshollanda@gmail.com>
2026-01-25 03:05:30 -0300

    hotfix mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,16 @@ import { NavLink } from "react-router-dom";
 import { MdMenu, MdClose } from "react-icons/md";
 import { useState } from "react";
 
-const Header = ({ children, dark, toggleTheme }) => {
+const Header = ({ children }) => {
   const [menuAberto, setMenuAberto] = useState(false);
 
   return (
     <div className={style.headerWrapper}>
       <header className={style.header}>
+        <div className={style.menu} onClick={() => setMenuAberto(!menuAberto)}>
+          {menuAberto ? <MdClose size={32} /> : <MdMenu size={32} />}
+        </div>
+
         <div className={style.logoArea}>
           <img src={logo} alt="Logo" className={style.logoIcon} />
         </div>
@@ -18,53 +22,45 @@ const Header = ({ children, dark, toggleTheme }) => {
           <NavLink
             to="/home"
             className={({ isActive }) =>
-              isActive ? style.active : style.inactive
+              `${style.link} ${isActive ? style.active : style.inactive}`
             }
             onClick={() => setMenuAberto(false)}
           >
             Início
           </NavLink>
+
           <NavLink
             to="/planos"
             className={({ isActive }) =>
-              isActive ? style.active : style.inactive
+              `${style.link} ${isActive ? style.active : style.inactive}`
             }
             onClick={() => setMenuAberto(false)}
           >
             Planos
           </NavLink>
+
           <NavLink
             to="/simulados"
             className={({ isActive }) =>
-              isActive ? style.active : style.inactive
+              `${style.link} ${isActive ? style.active : style.inactive}`
             }
             onClick={() => setMenuAberto(false)}
           >
             Simulados
           </NavLink>
+
           <NavLink
             to="/mentoria"
             className={({ isActive }) =>
-              isActive ? style.active : style.inactive
+              `${style.link} ${isActive ? style.active : style.inactive}`
             }
             onClick={() => setMenuAberto(false)}
           >
             MentorIA
           </NavLink>
-          {/* <NavLink
-                        to="/desempenho"
-                        className={({ isActive }) =>
-                            isActive ? style.active : style.inactive
-                        }
-                        onClick={() => setMenuAberto(false)}
-                    >
-                        Desempenho
-                    </NavLink> */}
         </nav>
 
-        <div className={style.botao}>
-          {children}
-        </div>
+        <div className={style.botao}>{children}</div>
       </header>
     </div>
   );
